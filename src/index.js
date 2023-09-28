f11fc2e92f8141c948aeca19cc59504d37199746
joshua chivile <joshuachivile@gmail.com>
2023-09-28 18:03:50 +0300

    add event listeners to to do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let form = document.querySelector("#create-task-form");
+  console.log(form);
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let individualTask = e.target.querySelector("#new-task-description").value;
+    taskList(individualTask);
+    form.reset();
+  });
+
+  const taskList = (todo) => {
+    let li = document.createElement('li');
+    let listParent = document.querySelector('#tasks')
+    let btn = document.createElement('button');
+    btn.textContent = ' X ';
+    li.textContent = todo;
+    li.appendChild(btn);
+    listParent.appendChild(li)
+    btn.addEventListener('click', () => {
+      btn.parentNode.remove();
+    })
+  };
 });
